5e03e69047ba7dabc3f789a698f31f43175d433a
thatanikett <aniketsingh4269@gmail.com>
2025-09-23 18:32:33 +0530

    added routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,22 @@
 import { Router } from "express"
-import {registerUser, LogoutUser} from "../controllers/user.controllers.js"
+import {
+    registerUser , 
+    loginUser,
+    LogoutUser,
+    changCurrentUserPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverimage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controllers.js"
 import {upload} from "../middlewares/multer.middlewares.js" 
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router()
 
+//UNSECURED ROUTES
 // post-> receiving the data 
 router.route("/register").post(
     upload.fields([
@@ -18,8 +30,18 @@ router.route("/register").post(
     ]),
     registerUser) //format -> router.route("path").httpmethod( middleware, controller, another controller...)
 
-//secured route
+Router.route("/login").post(verifyJWT, LoginUser)
+
+//SECURED ROUTES
 router.route("/logout").post(verifyJWT, LogoutUser)
+router.route("/change-password").post(verifyJWT, changCurrentUserPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+//because we using req.params
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar") , updateUserAvatar)
+router.route("/coverimage").patch(verifyJWT, upload.single("coverimage") , updateUserCoverimage)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 
 export default router
