a03ad8eaf3acab682652cebbd316e394a228756d
thatanikett <aniketsingh4269@gmail.com>
2025-09-24 21:42:47 +0530

    added routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import cookieParser from "cookie-parser"
 import {errorhandler} from "./middlewares/error.middlewares.js"
 import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscriptions.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 const app = express()
 
@@ -29,6 +33,10 @@ app.use("/api/v1/healthcheck",healthcheckRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/likes", likeRouter);
 
 
 app.use(errorhandler)
